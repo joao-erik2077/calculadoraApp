4ae684a0bcd9e19bbd319557af16764173ce5a6c
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-10-02 20:45:26 -0300

    tab1 design

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,9 +8,80 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  valor1: number;
-  valor2: number;
+  value1: number;
+  value2: number;
+  result: any;
+  operation: string;
 
-  constructor() {}
+  constructor(private toastController: ToastController) {}
 
+  verifyValues() {
+    return this.value1 && this.value2;
+  }
+
+  plus() {
+    if (this.verifyValues()) {
+      this.operation = `${this.value1} + ${this.value2}`;
+      this.result = this.value1 + this.value2;
+    } else {
+      this.failedVerification();
+    }
+  }
+
+  minus() {
+    if (this.verifyValues()) {
+      this.operation = `${this.value1} - ${this.value2}`;
+      this.result = this.value1 - this.value2;
+    } else {
+      this.failedVerification();
+    }
+  }
+
+  times() {
+    if (this.verifyValues()) {
+      this.operation = `${this.value1} * ${this.value2}`;
+      this.result = this.value1 * this.value2;
+    } else {
+      this.failedVerification();
+    }
+  }
+
+  divided() {
+    if (this.verifyValues()) {
+      this.operation = `${this.value1} / ${this.value2}`;
+      this.result = this.value1 / this.value2;
+    } else {
+      this.failedVerification();
+    }
+  }
+
+  greater() {
+    if (this.verifyValues()) {
+      this.operation = `${this.value1} > ${this.value2}`;
+      this.result = this.value1 > this.value2 ? 'Verdadeiro' : 'Falso';
+    } else {
+      this.failedVerification();
+    }
+  }
+
+  lower() {
+    if (this.verifyValues()) {
+      this.operation = `${this.value1} < ${this.value2}`;
+      this.result = this.value1 < this.value2 ? 'Verdadeiro' : 'Falso';
+    } else {
+      this.failedVerification();
+    }
+  }
+
+  async failedVerification() {
+    const toast = await this.toastController.create({
+      message: 'Insira os valores no campo',
+      duration: 1500,
+      color: 'danger',
+      position: 'bottom',
+      cssClass: 'failedVerificationToast'
+    });
+
+    await toast.present();
+  }
 }
